0be6f74193750e9bbbf915fd262fa81199bb66e4
Gabriel Aguilar <ubiquitousgabriel@gmail.com>
2023-10-25 19:29:06 -0400

    +double

---
+++ double.py
@@ -0,0 +1,6 @@
+def double(number):
+    return(number*2)
+
+uInput = input("Please give me a number:")
+uInput = int(uInput)
+print(double(uInput))
--- pluralize.py
+++ pluralize.py
@@ -1,5 +1,5 @@
 def pluralize(word):
     return(word+"s")
 
-userInput = input("Please give me a word:")
-print(pluralize(userInput))
+uInput = input("Please give me a word:")
+print(pluralize(uInput))
